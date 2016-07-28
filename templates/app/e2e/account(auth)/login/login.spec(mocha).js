cf2b50d9b2c998828ae4fb6086a3478c177e84df
Andrew Koroluk <koroluka@gmail.com>
2016-07-28 12:34:32 -0400

    test

--- templates/app/e2e/account(auth)/login/login.spec(mocha).js
+++ templates/app/e2e/account(auth)/login/login.spec(mocha).js
@@ -71,7 +71,7 @@ describe('Login View', function() {
         // Login takes some time, so wait until it's done.
         // For the test app's login, we know it's done when it
         // doesn't contain 'login'
-        return browser.wait(browser.driver.isElementPresent(by.id('banner')), 5000, 'Error: Element #banner did not display within 5 seconds');
+        return browser.wait(screenshot(i++).then(() => browser.driver.isElementPresent(by.id('banner'))), 5000, 'Error: Element #banner did not display within 5 seconds');
         // return screenshot(i++).then(() => browser.driver.wait(() => {
         //   return browser.driver.getCurrentUrl().then(url => {
         //     console.log(url);
